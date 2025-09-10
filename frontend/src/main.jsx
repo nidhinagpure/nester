495d410ff8b3a85a51203c32552f15d2dad7be68
nidhinagpure <nagpurenidhi8@gmail.com>
2025-09-10 23:44:50 +0530

    dhhd

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
-import ReactDOM  from "react-dom/client";
 import React from "react";
+import ReactDOM  from "react-dom/client";
 import ErrorBoundary from "./ErrorBoundary"
 import App from "./App";
 import "./style.css";
