3062683f4f551aaccacac176d4d361a8b1d17df9
nidhinagpure <nagpurenidhi8@gmail.com>
2025-09-10 23:07:12 +0530

    project delete

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import HeroSection from "../../components/HeroSection/HeroSection.jsx";
 import Gallary from "../../components/Gallary/Gallary.jsx";
