86ae386f189e7df2be3f8ba1f62c83200d840c5b
nidhinagpure <nagpurenidhi8@gmail.com>
2025-09-10 23:16:03 +0530

    djdj

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 
 import React from "react";
 import "./Home.css";
-import Navbar from "../../components/Navbar/Navbar.jsx";
-import HeroSection from "../../components/HeroSection/HeroSection.jsx";
-import Gallary from "../../components/Gallary/Gallary.jsx";
-import Card from "../../components/Properties/Properties.jsx";
-import Dearmsection from "./../../components/Dream/Dream.jsx";
-import Footer from "../../components/Footer/Footer.jsx";
+import Navbar from "./../../components/Navbar/Navbar.jsx";
+import HeroSection from "./../../components/HeroSection/HeroSection.jsx";
+import Gallary from "./../../components/Gallary/Gallary.jsx";
+import Card from "./../../components/Properties/Properties.jsx";
+import Dearmsection from "././../../components/Dream/Dream.jsx";
+import Footer from "./../../components/Footer/Footer.jsx";
 
 
 const Home = () => {
