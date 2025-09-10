d3b0ad19a1c0f9dc3ec0ea680ea4daf370b73bf1
nidhinagpure <nagpurenidhi8@gmail.com>
2025-09-10 23:31:20 +0530

    djddj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./style.css";
 
 import Home from "./pages/Home/Home.jsx";
